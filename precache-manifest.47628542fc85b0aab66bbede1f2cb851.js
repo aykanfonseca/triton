278526a20622dd4d880b95d62141c5b5a509c486
Aykan Fonseca <aykanfonseca5@gmail.com>
2018-10-29 22:24:30 -0700

    Updates

---
+++ precache-manifest.47628542fc85b0aab66bbede1f2cb851.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "8fdb0e2fa191ace2112cdebc10c1b349",
+    "url": "/triton/static/media/waitlist.8fdb0e2f.svg"
+  },
+  {
+    "revision": "5420360bb9e3ee116db4ae1e7721e1d3",
+    "url": "/triton/static/media/trident.5420360b.svg"
+  },
+  {
+    "revision": "5e7a2de92be4ab0df02916d112a29c2d",
+    "url": "/triton/static/media/search.5e7a2de9.svg"
+  },
+  {
+    "revision": "b88c9e866004e11f7001d1e2df05ac8b",
+    "url": "/triton/static/media/cancel.b88c9e86.svg"
+  },
+  {
+    "revision": "eb3f76c64ecf3c4f46c1",
+    "url": "/triton/static/js/runtime~main.eb3f76c6.js"
+  },
+  {
+    "revision": "62d3355c518463109e86",
+    "url": "/triton/static/js/main.62d3355c.chunk.js"
+  },
+  {
+    "revision": "a24a20a54b5abda80779",
+    "url": "/triton/static/js/1.a24a20a5.chunk.js"
+  },
+  {
+    "revision": "62d3355c518463109e86",
+    "url": "/triton/static/css/main.d361e1ad.chunk.css"
+  },
+  {
+    "revision": "21eaa8fb293459e33c300eb170316db8",
+    "url": "/triton/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f1ee51e8fa164800dde330f718515c46.js"
+  "/triton/precache-manifest.47628542fc85b0aab66bbede1f2cb851.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/triton/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
