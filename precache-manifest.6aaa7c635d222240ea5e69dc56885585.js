fb97cc401f1bb71c50b528bbbeabd49700ddc69e
Aykan Fonseca <aykanfonseca5@gmail.com>
2018-11-06 22:54:12 -0800

    Updates

---
+++ precache-manifest.6aaa7c635d222240ea5e69dc56885585.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "132438d3bb162dc9741f4f3f38c0467f",
+    "url": "/triton/static/media/cancel.132438d3.svg"
+  },
+  {
+    "revision": "f72bcb895f0e9c534ece",
+    "url": "/triton/static/css/main.5894b3fa.chunk.css"
+  },
+  {
+    "revision": "62a7bc6f5941c442a6ee",
+    "url": "/triton/static/js/1.62a7bc6f.chunk.js"
+  },
+  {
+    "revision": "eb3f76c64ecf3c4f46c1",
+    "url": "/triton/static/js/runtime~main.eb3f76c6.js"
+  },
+  {
+    "revision": "5420360bb9e3ee116db4ae1e7721e1d3",
+    "url": "/triton/static/media/trident.5420360b.svg"
+  },
+  {
+    "revision": "8fdb0e2fa191ace2112cdebc10c1b349",
+    "url": "/triton/static/media/waitlist.8fdb0e2f.svg"
+  },
+  {
+    "revision": "f72bcb895f0e9c534ece",
+    "url": "/triton/static/js/main.f72bcb89.chunk.js"
+  },
+  {
+    "revision": "5e7a2de92be4ab0df02916d112a29c2d",
+    "url": "/triton/static/media/search.5e7a2de9.svg"
+  },
+  {
+    "revision": "0b3b0b442303511901d9cbf16baf0163",
+    "url": "/triton/static/media/pin.0b3b0b44.svg"
+  },
+  {
+    "revision": "5174495f5eb6d02bbb751cdd2e44ccea",
+    "url": "/triton/static/media/help.5174495f.svg"
+  },
+  {
+    "revision": "3625a81466b0d9099e828e7bac9f68be",
+    "url": "/triton/static/media/settings.3625a814.svg"
+  },
+  {
+    "revision": "341f15c37a24592296ba39ac61e792a1",
+    "url": "/triton/static/media/email.341f15c3.svg"
+  },
+  {
+    "revision": "7daa47fb8254fd6a97510f487b4efb98",
+    "url": "/triton/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/triton/precache-manifest.328cf7c5e69642f68950327299700c34.js"
+  "/triton/precache-manifest.6aaa7c635d222240ea5e69dc56885585.js"
 );
 
 workbox.clientsClaim();
