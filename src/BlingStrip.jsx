3431dfd7606aad0f63485117e60bf3218127a2a0
Aykan Fonseca <aykanfonseca5@gmail.com>
2018-11-26 05:15:56 -0800

    Compressed the SVGs and fixed an issue with the dei tag in the bling strip.

--- src/BlingStrip.jsx
+++ src/BlingStrip.jsx
@@ -4,14 +4,14 @@ import { ReactComponent as WaitlistIcon } from './icons/tag_waitlist.svg';
 import { ReactComponent as DeiIcon } from './icons/tag_dei.svg';
 
 const Waitlist = () => (
-    <div className="bling-strip" title="Course is waitlisted" style={{backgroundColor: '#F7F7F7'}}>
+    <div className="bling-strip" title="Course is waitlisted" style={{width: '150px', backgroundColor: '#F7F7F7'}}>
         <WaitlistIcon />
         <b>Waitlisted</b>
     </div>
 );
 
 const Dei = () => (
-    <div className="bling-strip" title="A DEI approved course" style={{backgroundColor: '#EDF4FA'}}>
+    <div className="bling-strip" title="A DEI approved course" style={{width: '180px', backgroundColor: '#EDF4FA'}}>
         <DeiIcon />
         <b>DEI-approved</b>
     </div>
